0a9b718e86f4ca12d1ef3a9a336b4e7662136b5b
dirtymalikjin9k <hopemalik0512@gmail.com>
2023-05-21 09:37:29 +0300

    add unSubscribe api

--- app.py
+++ app.py
@@ -6,16 +6,26 @@ import time
 import hashlib
 from flask import Flask, render_template, request, redirect, url_for, jsonify, send_from_directory
 from flask_cors import CORS
-from ask import ask_ai, delete_collection
+from ask import ask_ai, delete_data_collection, delete_collection
 from psycopg2 import connect, extras
 from os import environ
 import json
 import shutil
+import stripe
+import datetime
+from dateutil.relativedelta import relativedelta
 
 
 app = Flask(__name__, static_folder='build')
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, resources={r"/api/*": {"origins": "https://1a78-65-109-52-221.ngrok-free.app/"}})
+
 os.makedirs("data", exist_ok=True)
+
+stripe.api_key = 'pk_test_51N8ikXCRd8rWbf0guJ5xqIR6c1Ya13PexdGenYTrru60C7nVLWrLxgX61ZAe55cDf53JmMKlurnS0Fb3GvIhIbfq00Su2SqotR'
+
+
+# endpoint_secret = 'whsec_ef236d754b0c2badbd37c064994eddfa7a630c790b8407b1395cd8727f4fee6a'
+endpoint_secret = 'whsec_Mxa8Ge5Ark73iJxU3FYXWQ4vysrnVA8Z'
 
 all_urls = set()
 
@@ -32,6 +42,13 @@ def get_connection():
                         user=user,
                         password=password)
     return conection
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 @app.post("/api/addData")
 def index():
@@ -152,7 +169,7 @@ def scrape_urls(urls, root_url, user_email, bot_id):
         urls_on_page = [urljoin(root_url, url) if not url.startswith(
             "http") else url for url in urls_on_page]
 
-        # Get the path of the URL
+        # Get the path of the URL 
         path = urljoin(root_url, url).replace(root_url, "").strip("/")
         if path == "":
             path = "index"
@@ -211,7 +228,7 @@ def api_chats_delte():
     print(requestInfo)
     user_email = requestInfo['user_email']
     bot_id = requestInfo['bot_id']
-    response = delete_collection(user_email, bot_id)
+    response = delete_data_collection(user_email, bot_id)
     return response
 
 @app.post('/api/botDelete')
@@ -237,6 +254,7 @@ def api_bot_delete():
             return "ok"
         except Exception as e:
             print('Error: ' + str(e))
+            return "ok"
 
 
 @app.post('/api/auth/register')
@@ -301,6 +319,7 @@ def api_auth_login():
 def api_newChat():
     requestInfo = request.get_json()
     email = requestInfo['user_email']
+    instance_name = requestInfo['instace_name']
     chat_name = requestInfo['chat_name']
     prompt = requestInfo['prompt']
     bot_id = requestInfo['bot_id']
@@ -310,6 +329,7 @@ def api_newChat():
         "answer": ""
     }]
 
+    print("urls_input = ", urls_input)
     # Extract the root URL from the first URL
 
     if email == '' or prompt == '' or bot_id == '' or urls_input== '' :
@@ -319,17 +339,38 @@ def api_newChat():
         cursor = connection.cursor(cursor_factory=extras.RealDictCursor)
         try:
             chats_str = json.dumps(chats)
-            cursor.execute('INSERT INTO chats (email, chat_name, prompt, urls, bot_id, chats) VALUES (%s, %s, %s, %s, %s, %s) RETURNING *',
-                        (email, chat_name, prompt, urls_input, bot_id, chats_str))
+            cursor.execute('INSERT INTO chats (email, instance_name, chat_name, prompt, urls, bot_id, chats, complete) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING *',
+                        (email, instance_name, chat_name, prompt, urls_input, bot_id, chats_str, 'false'))
             new_created_chat = cursor.fetchone()
+            connection.commit()
             print(new_created_chat)
-            
+
+            cursor.execute('SELECT * FROM subscription where email = %s', (email ,))
+            subscription = cursor.fetchone()
+            connection.commit()
+
+            urls = [url.strip() for url in urls_input.split(",")]
+
+            if(subscription is None):
+                urls = urls[:2]
+            else: 
+                end_time = datetime.datetime.fromtimestamp(int(subscription['end_date']))
+                current_time = datetime.datetime.now()
+                
+                if(current_time > end_time):
+                    urls = urls[:2]
+
+            print(len(urls))
+            root_url = urljoin(urls[0], "/")
+            scrape_urls(urls, root_url, email, bot_id)
+
+            cursor.execute('UPDATE chats SET complete = %s WHERE email = %s AND bot_id = %s', ('true', email, bot_id))
+            # new_created_chat = cursor.fetchone()
+
             connection.commit()
             cursor.close()
             connection.close()
-            urls = [url.strip() for url in urls_input.split(",")]
-            root_url = urljoin(urls[0], "/")
-            scrape_urls(urls, root_url, email, bot_id)
+
             return "ok"
         except Exception as e:
             print('Error: '+ str(e))
@@ -339,12 +380,13 @@ def api_newChat():
 def api_updateChat():
     requestInfo = request.get_json()
     email = requestInfo['user_email']
+    instance_name = requestInfo['instance_name']
     chat_name = requestInfo['chat_name']
     prompt = requestInfo['prompt']
     bot_id = requestInfo['bot_id']
     urls_input = requestInfo['urls_input']
     custom_text = requestInfo['custom_text']
-    if email == '' or prompt == '' or bot_id == '' or urls_input== '' :
+    if email == '' or instance_name == '' or prompt == '' or bot_id == '' or urls_input== '' :
         return {}
     else:
         user_email_hash = create_hash(email)
@@ -354,12 +396,11 @@ def api_updateChat():
         connection = get_connection()
         cursor = connection.cursor(cursor_factory=extras.RealDictCursor)
         try:
-            cursor.execute("UPDATE chats SET chat_name = %s, prompt = %s, urls = %s, custom_text = %s WHERE email = %s AND bot_id = %s",
-                    (chat_name, prompt, urls_input, custom_text, email, bot_id))
-            
+            cursor.execute("UPDATE chats SET instance_name = %s, chat_name = %s, prompt = %s, urls = %s, custom_text = %s, complete = %s WHERE email = %s AND bot_id = %s",
+                    (instance_name, chat_name, prompt, urls_input, custom_text, 'false', email, bot_id))
             connection.commit()
-            cursor.close()
-            connection.close()
+
+
             urls = [url.strip() for url in urls_input.split(",")]
             root_url = urljoin(urls[0], "/")
             scrape_urls(urls, root_url, email, bot_id)
@@ -367,6 +408,13 @@ def api_updateChat():
                 filename = f"{data_directory}/custom_text.txt"
                 with open(filename, "w") as file:
                     file.write(custom_text)
+            response = delete_data_collection(email, bot_id)
+            cursor.execute("UPDATE chats SET instance_name = %s, chat_name = %s, prompt = %s, urls = %s, custom_text = %s, complete = %s WHERE email = %s AND bot_id = %s",
+                    (instance_name, chat_name, prompt, urls_input, custom_text, 'false', email, bot_id))
+
+            cursor.close()
+            connection.close()
+
             return "ok"
         except Exception as e:
             print('Error: '+ str(e))
@@ -394,7 +442,139 @@ def api_getChatInfos():
         except Exception as e:
             print('Error: '+ str(e))
             return jsonify({'message': 'chat does not exist'}), 404
-    
+ 
+@app.post('/api/webhook')
+def api_webhook():
+    event = None
+    payload = request.data
+    if endpoint_secret:
+        sig_header = request.headers.get('Stripe-Signature')
+        try:
+            event = stripe.Webhook.construct_event(
+                payload, sig_header, endpoint_secret
+            )
+        except stripe.error.SignatureVerificationError as e:
+            print('⚠️  Webhook signature verification failed.' + str(e))
+            return jsonify(success=False)
+        
+    # Handle the event
+     # Handle the event
+    charge = session = invoice = None
+    if event['type'] == 'checkout.session.completed':
+      session = event['data']['object']
+      print("session = ",session)
+    elif event['type'] == 'charge.succeeded':
+      charge = event['data']['object']
+      print("charge = ",charge)
+    elif event['type'] == 'invoice.paid':
+      invoice = event['data']['object']
+      print("invoice = ",invoice)
+    # ... handle other event types
+    else:
+      print('Unhandled event type {}'.format(event['type']))
+
+    if charge : 
+        connection = get_connection()
+        cursor = connection.cursor(cursor_factory=extras.RealDictCursor)
+
+        email = charge['billing_details']['email']
+
+        payment_method = charge['payment_method']
+
+        if(charge['amount'] == 2000):
+            type = 2
+        else:
+            type = 1
+
+        start_date = charge['created']
+
+        date_obj = datetime.datetime.utcfromtimestamp(start_date)
+
+        end_date_obj = date_obj + relativedelta(months=1)
+
+        end_date = int(end_date_obj.timestamp())
+        
+        cursor.execute('INSERT INTO subscription(email, payment_method, type, start_date, end_date) VALUES (%s, %s, %s, %s, %s) RETURNING *',
+                    (email, payment_method, type, start_date, end_date))
+        new_created_user = cursor.fetchone()
+        print(new_created_user)
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    return jsonify(success=True)
+
+@app.post('/api/getSubscription')
+def api_getSubscription():
+    requestInfo = request.get_json()
+    email = requestInfo['user_email']
+    if email == '':
+        return "ok"
+    else:
+        connection = get_connection()
+        cursor = connection.cursor(cursor_factory=extras.RealDictCursor)
+        # try:
+        cursor.execute('SELECT * FROM subscription WHERE email = %s ', (email,))
+
+        selects = cursor.fetchall()
+        connection.commit()
+
+        print(selects)
+        
+        if(len(selects) == 0) :
+            return '1'
+        else:
+            subscription = selects[len(selects)-1]
+            print("subscription = ", subscription)
+            end_time = datetime.datetime.fromtimestamp(int(subscription['end_date']))
+            current_time = datetime.datetime.now()
+            
+            if end_time > current_time:
+                if subscription['type'] == 1:
+                    cursor.close()
+                    connection.close()
+                    return '5'
+                else: 
+                    cursor.close()
+                    connection.close()
+                    return '10'
+            else:
+                cursor.execute('DELETE FROM subscription WHERE email = %s ',
+                                (email, ))            
+                connection.commit()
+                
+                cursor.execute('DELETE FROM chats WHERE email = %s ',
+                                (email, ))            
+                connection.commit()
+
+                cursor.close()
+                connection.close()
+                return '1'
+
+@app.post('/api/unSubscribe')
+def api_unsubscribe():
+    requestInfo = request.get_json()
+    email = requestInfo['user_email']
+    if email == '':
+        return "ok"
+    else:
+        data_directory = f"data/{create_hash(email)}"
+        shutil.rmtree(data_directory)
+        connection = get_connection()
+        cursor = connection.cursor(cursor_factory=extras.RealDictCursor)
+        # try:
+        cursor.execute('DELETE FROM subscription WHERE email = %s',
+                                (email,))            
+        connection.commit()
+
+        # cursor.execute('DELETE FROM chats WHERE email = %s',
+        #                         (email,))            
+        # connection.commit()
+        response = delete_collection(email, connection, cursor)
+        cursor.close()
+        connection.close()
+        return "ok"
 
 def create_hash(text):
     return hashlib.md5(text.encode()).hexdigest()
@@ -407,6 +587,8 @@ def serve(path):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
