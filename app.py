4f10d5b6896ec37f26460242bfe65acae7c6c86c
dirtymalikjin9k <hopemalik0512@gmail.com>
2023-05-23 04:44:39 +0300

    return user in loginapi

--- app.py
+++ app.py
@@ -314,7 +314,7 @@ def api_auth_login():
             if user is None:
                 print(user)
                 return jsonify({'message': 'Email or Password does not correct'}), 404
-            return jsonify({'data': user}), 200
+            return user
         except:
             return jsonify({'message': 'Email or Password does not correct'}), 404
 
