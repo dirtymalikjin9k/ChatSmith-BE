94e40861d9c3dcd4fdd44e253c998cb3b13f92f4
dirtymalikjin9k <hopemalik0512@gmail.com>
2023-09-05 13:18:38 +0300

    set debug false

--- app.py
+++ app.py
@@ -1423,4 +1423,4 @@ def run():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
